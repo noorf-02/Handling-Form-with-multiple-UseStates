5c519863dcd75924a9967d411fc9b2235cf5d8d9
Noor Fatima <noor2239064@gmail.com>
2026-07-08 10:59:41 +0500

    set up the frontend

--- src/COMPONENTS/FormComponent.jsx
+++ src/COMPONENTS/FormComponent.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 function FormComponent() {
   return (
-    <div>
-      FORM COMPONENT
+    <div className='form-container wrapper my-3'>
+        <h1 className='text-4xl font-bold text-center'>Form with Multiple States</h1>
+      <form action="" className='flex flex-col gap-3 items-center my-10'>
+
+        <div className='flex flex-col gap-2'>
+            <label htmlFor="name" className='text-[20px] font-medium'>Enter Full Name</label>
+            <input type="text" className='border-1 w-[350px] py-2 px-4 rounded-3xl focus:outline-none border-gray-300' id='name' name='name' placeholder='John Doe' />
+        </div>
+
+        <div className='flex flex-col gap-2'>
+            <label htmlFor="address" className='text-[20px] font-medium'>Enter Home Address</label>
+            <input type="text" className='border-1 w-[350px] py-2 px-4 rounded-3xl focus:outline-none border-gray-300' id='address' name='address' placeholder='Street 05 / Apt. 78' />
+        </div>
+
+        <div className='flex flex-col gap-2'>
+            <label htmlFor="contact" className='text-[20px] font-medium'>Enter Contact</label>
+            <input type="text" className='border-1 w-[350px] py-2 px-4 rounded-3xl focus:outline-none border-gray-300' id='contact' name='contact' placeholder='+91 893329123' />
+        </div>
+
+      </form>
     </div>
   )
 }
